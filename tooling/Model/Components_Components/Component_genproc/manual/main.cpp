4664c2b66f649c97586312e9a1c51ee27d8d8db2
Thomas Spitzer <t.spitzer@consulting.astade.de>
2016-02-14 08:23:14 +0100

    fixed the command line parameters

--- tooling/Model/Components_Components/Component_genproc/manual/main.cpp
+++ tooling/Model/Components_Components/Component_genproc/manual/main.cpp
@@ -20,13 +20,10 @@ namespace ascii = boost::spirit::ascii;
 /* This structure is used by main to communicate with parse_opt. */
 struct Arguments
 {
-    Arguments(): verbose(false), beautify(false), scriptfile(0), xmlfile(0), host(0), port(0) {}
+    Arguments(): verbose(false), beautify(false) {}
     bool verbose;
     bool beautify;
-    char* scriptfile;
-    char* xmlfile;
-    char* host;
-    int port;
+    std::vector<std::string> file_names;
 };
 
 Arguments arguments;
@@ -55,13 +52,9 @@ const char *argp_program_bug_address =
 
 static struct argp_option options[] =
 {
-  {"host",    'h', "HOST ADDR",  0, "the tcp address of the target.\n(e.g.: localhost or 127.0.0.1)"},
-  {"port",    'p', "HOST PORT",  0, "the port number to connect.\n(e.g.: 23)"},
-  {"script",  's', "SCRIPTFILE", 0, "scriptfile to execute"},
   {"verbose", 'v', 0,            0, "verbose info aboout parsing."},
-  {"trace",   't', 0,            0, "an additional debug trace (for the testrunner)."},
   {"beautify",'b', 0,            0, "output the parsed text in a beautified form."},
-  {"xml"     ,'x', "XML FILE",   0, "output the test report in xml format to this file."},
+  {"input"   ,'i', "file",       0, "the input file (May be set multiple times)."},
   {0}
 };
 
@@ -77,17 +70,8 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
     case 'v':
         arguments->verbose = true;
         break;
-    case 's':
-        arguments->scriptfile = arg;
-        break;
-    case 'h':
-        arguments->host = arg;
-        break;
-    case 'x':
-        arguments->xmlfile = arg;
-        break;
-    case 'p':
-        arguments->port = atoi(arg);
+    case 'i':
+        arguments->file_names.push_back(arg);
         break;
 
     default:
@@ -105,64 +89,66 @@ int main (int argc, char **argv)
 {
     /* Where the magic happens */
     argp_parse (&argp, argc, argv, 0, 0, &arguments);
-
-    if (!arguments.scriptfile)
-    {
-        printf("you must specify a script file\n");
-        exit(1);
-    }
-
-    // open file, disable skipping of whitespace
-    std::ifstream in(arguments.scriptfile);
-    in.unsetf(std::ios::skipws);
-
-    // wrap istream into iterator
-    boost::spirit::istream_iterator begin(in);
-    boost::spirit::istream_iterator end;
+    std::vector<ast::root_element> ast;
 
     // wrap forward iterator with position iterator, to record the position
     typedef classic::position_iterator2<boost::spirit::istream_iterator>
         pos_iterator_type;
 
-    pos_iterator_type position_begin(begin, end, arguments.scriptfile);
-    pos_iterator_type position_end;
-
-    testscript<pos_iterator_type> p;       // create instance of parser
-
-    try
+    if (arguments.file_names.empty())
     {
-        std::vector<ast::root_element> ast;
-        qi::phrase_parse(position_begin, position_end, p, qi::space, ast);
-        if (position_begin != position_end)
-            throw qi::expectation_failure<pos_iterator_type>(position_begin, position_end,boost::spirit::info("general error"));
+        printf("you must specify a process file\n");
+        exit(1);
     }
-    catch(const qi::expectation_failure<pos_iterator_type> e)
+    else for (std::vector<std::string>::iterator it = arguments.file_names.begin(); it != arguments.file_names.end(); it++)
     {
-        const classic::file_position_base<std::string>& pos = e.first.get_position();
-        std::cout   << "parse error at file "
-                    << pos.file 
-                    << " line " 
-                    << pos.line 
-                    << " column " 
-                    << pos.column 
-                    << std::endl
-                    << e.what_
-                    << std::endl
-                    << "'"
-                    << e.first.get_currentline() 
-                    << "'" << std::endl 
-                    << std::setw(pos.column) 
-                    << " " 
-                    << "^- here" 
-                    << std::endl;
-        return -1;    
-    }
-    catch(std::string e)
-    {
-        std::cout   << "logical error: "
-                    << e
-                    << std::endl;
-        return -1;
+        try
+        {
+            // open file, disable skipping of whitespace
+            std::ifstream in((*it).c_str());
+            in.unsetf(std::ios::skipws);
+
+            // wrap istream into iterator
+            boost::spirit::istream_iterator begin(in);
+            boost::spirit::istream_iterator end;
+
+            pos_iterator_type position_begin(begin, end, *it);
+            pos_iterator_type position_end;
+
+            testscript<pos_iterator_type> p;       // create instance of parser
+
+            qi::phrase_parse(position_begin, position_end, p, qi::space, ast);
+            if (position_begin != position_end)
+                throw qi::expectation_failure<pos_iterator_type>(position_begin, position_end,boost::spirit::info("general error"));
+        }
+        catch(const qi::expectation_failure<pos_iterator_type> e)
+        {
+            const classic::file_position_base<std::string>& pos = e.first.get_position();
+            std::cout   << "parse error at file "
+                        << pos.file 
+                        << " line " 
+                        << pos.line 
+                        << " column " 
+                        << pos.column 
+                        << std::endl
+                        << e.what_
+                        << std::endl
+                        << "'"
+                        << e.first.get_currentline() 
+                        << "'" << std::endl 
+                        << std::setw(pos.column) 
+                        << " " 
+                        << "^- here" 
+                        << std::endl;
+            return -1;    
+        }
+        catch(std::string e)
+        {
+            std::cout   << "logical error: "
+                        << e
+                        << std::endl;
+            return -1;
+        }
     }
 
     return 0;
