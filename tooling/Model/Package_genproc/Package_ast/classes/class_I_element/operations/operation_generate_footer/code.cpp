83143188a6ac05323ecaeca92001b483b58dac9a
Thomas Spitzer <rwe.astade.de>
2016-02-25 16:57:22 +0100

    implemented a new pattern

--- tooling/Model/Package_genproc/Package_ast/classes/class_I_element/operations/operation_generate_footer/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_I_element/operations/operation_generate_footer/code.cpp
@@ -1,3 +1,11 @@
 //~~ void generate_footer(std::ofstream& file) [I_element] ~~
+file << "</td>" << std::endl;
+file << "</tr>" << std::endl;
+file << "<tr class=\"footer\">" << std::endl;
+file << "<td class=\"footer\">" << std::endl;
+file << "<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>" << std::endl;
+file << "</td>" << std::endl;
+file << "</tr>" << std::endl;
+file << "</table>" << std::endl;
 file << "</body>" << std::endl;
 file << "</html>" << std::endl;
--- tooling/Model/Package_genproc/Package_ast/classes/class_I_element/operations/operation_generate_header/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_I_element/operations/operation_generate_header/code.cpp
@@ -1,16 +1,29 @@
 //~~ void generate_header(std::ofstream& file) [I_element] ~~
-file << "<!DOCTYPE html>" << std::endl;
-file << "<html lang=\"en\">" << std::endl;
+file << "<!DOCTYPE>" << std::endl;
+file << "<html lang=\"en\" manifest=\"automotive-scrum.appcache\">" << std::endl;
 file << "<head>" << std::endl;
 file << "<title>" << getType() << " " << getLabel() << "</title>" << std::endl;
 file << "<link rel=\"stylesheet\" href=\"style.css\">" << std::endl;
 file << "<META HTTP-EQUIV=\"Content-Type\" content=\"text/html; charset=utf-8\">" << std::endl;
 file << "</head>" << std::endl;
+file << "<body>" << std::endl;
+file << "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">" << std::endl;
 
-file << "<body class=\"news\">" << std::endl;
-file << "  <header>" << std::endl;
+file << "<tr class=\"header\">" << std::endl;
+file << "<td>" << std::endl;
+file << "<table border=\"0\" cellpadding=\"10\" cellspacing=\"0\" width=\"100%\">" << std::endl;
+file << "<tr>" << std::endl;
+file << "<td>" << std::endl;
+file << "<div><img src=\"automotive_scrum_logo_small.png\"></div>" << std::endl;
+file << "</td>" << std::endl;
+file << "<td>" << std::endl;
+file << "<p class=\"header\">Lorem ipsum.</p>" << std::endl;
+file << "</td>" << std::endl;
+file << "</tr>" << std::endl;
+file << "</table>" << std::endl;
+file << "</td>" << std::endl;
+file << "</tr>" << std::endl;
 
-file << "  <div id=\"my_logo\"><img src=\"automotive_scrum_logo_small.png\"></div>" << std::endl;
 
-
-file << "  </header>" << std::endl;
+file << "<tr class=\"body\">" << std::endl;
+file << "<td>" << std::endl;
