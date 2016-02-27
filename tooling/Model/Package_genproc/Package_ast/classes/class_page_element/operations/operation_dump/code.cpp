fd73d0dd67eab26179a464467324685c2ec7c16a
Thomas Spitzer <t.spitzer@consulting.astade.de>
2016-02-27 07:57:59 +0100

    dump supports label

--- tooling/Model/Package_genproc/Package_ast/classes/class_page_element/operations/operation_dump/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_page_element/operations/operation_dump/code.cpp
@@ -1,5 +1,7 @@
 //~~ void dump() [page_element] ~~
 std::cout << "page " << m_identifier << " {" << std::endl;
+if (m_identifier != m_label)
+    std::cout << "\tlabel \"" << m_label << "\";" << std::endl;
 subpage_owner::dump();
 dump_text();
 std::cout << "}" << std::endl;
