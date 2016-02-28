13b9a64f25cf59bcfaa075c558e26cc91ab9e919
Thomas Spitzer <t.spitzer@consulting.astade.de>
2016-02-28 12:37:43 +0100

    did some size adjustment

--- tooling/Model/Package_genproc/Package_ast/classes/class_activity_element/operations/operation_get_graph_node/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_activity_element/operations/operation_get_graph_node/code.cpp
@@ -1,7 +1,9 @@
 //~~ std::string get_graph_node() [activity_element] ~~
 std::string ret;
 ret += label_owner::getIdentifier()
-    + "[shape=cds, style=filled, color=\"#A57521\", fillcolor=\"#FCDBA2\", fontcolor=\"#7B4E00\", label=\""
+    + "["
+    + graphics_creator::text_standards
+    + "shape=house, orientation=270, style=filled, color=\"#A57521\", fillcolor=\"#FCDBA2\", fontcolor=\"#7B4E00\", label=\""
     + label_owner::getLabel()
     + "\", URL=\""
     + getHtmlFilename()
--- tooling/Model/Package_genproc/Package_ast/classes/class_process_element/operations/operation_get_graph_node/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_process_element/operations/operation_get_graph_node/code.cpp
@@ -1,7 +1,9 @@
 //~~ std::string get_graph_node() [process_element] ~~
 std::string ret;
 ret += label_owner::getIdentifier()
-    + "[shape=box3d, style=filled, color=\"#A57521\", fillcolor=\"#7B4E00\", fontcolor=\"#FCDBA2\", label=\""
+    + "["
+    + graphics_creator::text_standards
+    + "shape=box3d, style=filled, color=\"#A57521\", fillcolor=\"#7B4E00\", fontcolor=\"#FCDBA2\", label=\""
     + label_owner::getLabel()
     + "\", URL=\""
     + getHtmlFilename()
