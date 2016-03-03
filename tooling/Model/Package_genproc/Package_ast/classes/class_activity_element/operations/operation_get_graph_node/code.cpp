1fe37034981af503ff2e016380c45422f9e5f6e3
Thomas Spitzer <rwe.astade.de>
2016-03-03 18:10:32 +0100

    fixed the mergin

--- tooling/Model/Package_genproc/Package_ast/classes/class_activity_element/operations/operation_get_graph_node/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_activity_element/operations/operation_get_graph_node/code.cpp
@@ -12,7 +12,7 @@ ret += label_owner::getIdentifier()
     + graphics_creator::text_standards
     + "shape=house, orientation=270, style=filled, color=\"#A57521\", fillcolor=\""
     + fc
-    + "\", fontcolor=\"#7B4E00\", margin=\"0.1\", label=\""
+    + "\", fontcolor=\"#7B4E00\", label=\""
     + breakApart(label_owner::getLabel())
     + "\", "
     + "tooltip=\""
