4f19e5da31d50a5d257fdc8cd828aab08684facf
Thomas Spitzer <t.spitzer@consulting.astade.de>
2016-03-04 12:30:18 +0100

    made "rolr" a better node

--- tooling/Model/Package_genproc/Package_ast/classes/class_SLLKFLAL/operations/operation_NWEBWARH/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_SLLKFLAL/operations/operation_NWEBWARH/code.cpp
@@ -5,10 +5,8 @@ std::string fc = "#FCB7A2";
 ret += label_owner::getIdentifier()
     + "["
     + graphics_creator::text_standards
-    + "shape=doublecircle, style=filled, color=\"#7B1E00\", fillcolor=\""
-    + fc
-    + "\", fontcolor=\"#7B1E00\", label=\""
-    + breakApart(label_owner::getLabel())
+    + "shape=box, image=\"role.png\", color=white, fontcolor=\"#7B1E00\", label=\""
+    + "\\n\\n\\n\\n\\n\\n" + breakApart(label_owner::getLabel())
     + "\", "
     + "tooltip=\""
     + label_owner::getBrief()
