4faa94fec8b39cbe859817d55aa16cb94c7ae0e0
Thomas Spitzer <t.spitzer@consulting.astade.de>
2016-03-04 12:38:33 +0100

    automatically draw inditect responsibilities

--- tooling/Model/Package_genproc/Package_ast/classes/class_JOPOEADW/operations/operation_ENGDNJLG/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_JOPOEADW/operations/operation_ENGDNJLG/code.cpp
@@ -1,4 +1,17 @@
 //~~ ast::I_element* getRole(bool* IsDirectChild = 0) [role_owner] ~~
 if (IsDirectChild)
-    *IsDirectChild = true;
-return my_role;
+    *IsDirectChild = my_role != 0;
+
+if (my_role != 0)
+    return my_role;
+    
+const parent_owner* p = dynamic_cast<const parent_owner*>(this);
+if (p == 0)
+    return 0;
+    
+const role_owner* r = dynamic_cast<const role_owner*>(p->getParent());
+
+if (r)
+    return r->getRole();
+else
+    return 0;
