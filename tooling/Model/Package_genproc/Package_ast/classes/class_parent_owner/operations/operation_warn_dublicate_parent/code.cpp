a9b35f4205f7744c9e15294fe96f3da7b3cbcc1c
Thomas Spitzer <t.spitzer@consulting.astade.de>
2016-03-12 18:30:44 +0100

    fixed a namespace bug

--- tooling/Model/Package_genproc/Package_ast/classes/class_parent_owner/operations/operation_warn_dublicate_parent/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_parent_owner/operations/operation_warn_dublicate_parent/code.cpp
@@ -1,10 +1,13 @@
 //~~ void warn_dublicate_parent(I_element* newParent) [parent_owner] ~~
 const I_element* me = dynamic_cast<const I_element*>(this);
 std::cerr   << "Warning: you try to set "
+            << newParent->getFullPath()
             << newParent->getIdentifier()
             << " as parent of "
+            << me->getFullPath()
             << me->getIdentifier()
             << " which has already "
+            << my_parent->getFullPath()
             << my_parent->getIdentifier()
             << " as parent. So it is ignored."
             << std::endl;
