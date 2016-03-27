d6b7d8e6daca3258bb5749015ebc4ad5111ac651
Thomas Spitzer <t.spitzer@consulting.astade.de>
2016-03-27 16:16:40 +0200

    increased the depth of artefact breakdown

--- tooling/Model/Package_genproc/Package_ast/classes/class_graphics_creator/operations/operation_create_artefact_breakdown/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_graphics_creator/operations/operation_create_artefact_breakdown/code.cpp
@@ -1,6 +1,7 @@
 //~~ void create_artefact_breakdown(std::ofstream& outfile, const std::string& outpath) [graphics_creator] ~~
 const I_element* e = dynamic_cast<const I_element*>(this);
 const parent_owner* p = dynamic_cast<const parent_owner*>(this);
+const subpage_owner* s = dynamic_cast<const subpage_owner*>(this);
 
 FILE *gfile;
 std::string linkname = e->getFullIdentifier() + "_artefact_breakdown";
@@ -16,7 +17,12 @@ if(!(gfile = popen(command.c_str(), "w")))
 fputs("digraph artefact_breakdown {\n", gfile);
 fputs("rankdir=\"LR\";\n", gfile);
 
-if (p)
+int i = 3;
+
+if ((s==0) || (!s->hasSubpages()))
+    i=4;
+
+while (p && (i-- > 0))
 {
     const graphics_creator* g = dynamic_cast<const graphics_creator*>(p->getParent());
     const process_element* pr = dynamic_cast<const process_element*>(p->getParent());
@@ -30,6 +36,10 @@ if (p)
         g->insert_child_nodes(gfile);
         fputs(p->getParent()->get_graph_node(false).c_str(),gfile);
     }
+    else 
+        i = 0;
+
+    p = dynamic_cast<const parent_owner*>(p->getParent());
 }
 
 fputs(e->get_graph_node(true).c_str(),gfile);
