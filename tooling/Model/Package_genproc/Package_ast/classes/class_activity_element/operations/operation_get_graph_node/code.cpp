83197488076a94419149e9b6e2638878e5f8e774
Thomas Spitzer <t.spitzer@consulting.astade.de>
2016-04-03 10:01:19 +0200

    some refactoring in the process description

--- tooling/Model/Package_genproc/Package_ast/classes/class_activity_element/operations/operation_get_graph_node/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_activity_element/operations/operation_get_graph_node/code.cpp
@@ -10,7 +10,7 @@ else
 ret += getFullIdentifier()
     + "["
     + graphics_creator::text_standards
-    + "shape=house, orientation=270, margin=\"0\", style=filled, color=\"#A57521\", fillcolor=\""
+    + "shape=house, orientation=270, margin=\"0.04\", style=filled, color=\"#A57521\", fillcolor=\""
     + fc
     + "\", fontcolor=\"#7B4E00\", label=\""
     + breakApart(label_owner::getLabel())
