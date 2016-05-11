461bdbe3a80de8346daa5478f944d247c157716c
john22222 <post09@astade.de>
2016-05-11 14:58:46 +0200

    Update code.cpp

--- tooling/Model/Package_genproc/Package_ast/classes/class_graphics_creator/operations/operation_arrow_transform/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_graphics_creator/operations/operation_arrow_transform/code.cpp
@@ -1,4 +1,4 @@
 //~~ std::string arrow_transform() [graphics_creator] ~~
 return std::string("[arrowhead=\"normal\", arrowtail=\"none\", dir=\"both\", ")  +  
             text_standards  + 
-            "fontcolor=\"#072551\", color=\"#7B1E00\", label=\"transform\"];";
+            "fontcolor=\"#072551\", color=\"#7B1E00\"];";
