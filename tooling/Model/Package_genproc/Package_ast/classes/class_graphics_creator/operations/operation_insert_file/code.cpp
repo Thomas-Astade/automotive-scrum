8a1cfb8abaea2d4246fb4a4c6f7196b88907172d
thomas <t.spitzer@astade.de>
2016-11-03 10:35:03 +0100

    removed id=node from clickmap

--- tooling/Model/Package_genproc/Package_ast/classes/class_graphics_creator/operations/operation_insert_file/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_graphics_creator/operations/operation_insert_file/code.cpp
@@ -5,9 +5,20 @@ char buff[512];
 if(!(pFile = fopen (sourcename.c_str(),"r")))
     throw(sourcename);
 
+std::string obuf;
+
 while(fgets(buff, sizeof(buff), pFile)!=NULL)
 {
-    file << buff;
+    obuf += buff;
 }
+
+while (obuf.find("id=\"node")!=std::string::npos)
+{
+    int p1 = obuf.find("id=\"node");
+    int p2 = obuf.find("\"",p1);
+    obuf.erase(p1,2+p2-p1);
+}
+
+file << obuf;
 
 fclose(pFile);
