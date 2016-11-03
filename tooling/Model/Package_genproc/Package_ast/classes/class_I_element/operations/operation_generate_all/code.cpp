c50918ae41dacf4b81a4ae9b1674e7c7826b1291
thomas <t.spitzer@astade.de>
2016-11-03 08:26:23 +0100

    add the changed date to the sitemap

--- tooling/Model/Package_genproc/Package_ast/classes/class_I_element/operations/operation_generate_all/code.cpp
+++ tooling/Model/Package_genproc/Package_ast/classes/class_I_element/operations/operation_generate_all/code.cpp
@@ -1,21 +1,38 @@
 //~~ void generate_all(const std::string& outpath) [I_element] ~~
+for (std::map<std::string,I_element*>::iterator it = object_list.begin(); it != object_list.end(); it++)
+{
+    (*it).second->generate(outpath);
+}
+
 std::ofstream ofs;
 std::string fullName = outpath+"/sitemap.xml";
 ofs.open (fullName.c_str(), std::ofstream::out);
+
+time_t rawtime;
+struct tm * timeinfo;
+char buffer[80];
+
+time (&rawtime);
+timeinfo = localtime(&rawtime);
+
+strftime(buffer,80,"%Y-%m_%d",timeinfo);
 
 ofs << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << std::endl;
 ofs << "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" << std::endl;
 
 for (std::map<std::string,I_element*>::iterator it = object_list.begin(); it != object_list.end(); it++)
 {
-    (*it).second->generate(outpath);
-
     ofs << "<url>" << std::endl;
+    
     ofs << "<loc>http://automotive-scrum.org/"
         << (*it).second->getHtmlFilename()
         << "</loc>" << std::endl;
+        
+    ofs << "<lastmod>"
+        << buffer
+        << "1</lastmod>" << std::endl;
+        
     ofs << "</url>" << std::endl;
-
 }
 
 std::cout << "created " << object_list.size() << " elements." << std::endl;
